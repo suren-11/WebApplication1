063492b1b12f090c734be3e41752baa3d93c0352
suren-11 <presanth11@gmail.com>
2023-07-18 21:41:44 +0530

    Product to Product Dto

--- WebApplication1/Controllers/ProductsController.cs
+++ WebApplication1/Controllers/ProductsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Data;
 using WebApplication1.Models;
+using WebApplication1.Models.Dto;
 
 namespace WebApplication1.Controllers
 {
@@ -10,7 +11,7 @@ namespace WebApplication1.Controllers
     public class ProductsController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Product> GetAllProducts()
+        public IEnumerable<ProductDto> GetAllProducts()
         {
             var products = ProductDB.ProductList;
             return products;
--- WebApplication1/Data/ProductDB.cs
+++ WebApplication1/Data/ProductDB.cs
@@ -1,12 +1,13 @@
 ﻿using WebApplication1.Models;
+using WebApplication1.Models.Dto;
 
 namespace WebApplication1.Data
 {
     public static class ProductDB
     {
-        public static List<Product> ProductList = new List<Product>() {
-                new Product{ Id = 1, Name = "product1" },
-                new Product{ Id = 2, Name = "product2" }
+        public static List<ProductDto> ProductList = new List<ProductDto>() {
+                new ProductDto{ Id = 1, Name = "product1" },
+                new ProductDto{ Id = 2, Name = "product2" }
             };
     }
 }
