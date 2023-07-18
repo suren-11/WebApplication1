967453ba565322126c227443e12a6b996ef701c4
suren-11 <presanth11@gmail.com>
2023-07-18 21:34:37 +0530

    Refer with static list

--- WebApplication1/Controllers/ProductsController.cs
+++ WebApplication1/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -7,5 +9,11 @@ namespace WebApplication1.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            var products = ProductDB.ProductList;
+            return products;
+        }
     }
 }
---
+++ WebApplication1/Data/ProductDB.cs
@@ -0,0 +1,12 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public static class ProductDB
+    {
+        public static List<Product> ProductList = new List<Product>() {
+                new Product{ Id = 1, Name = "product1" },
+                new Product{ Id = 2, Name = "product2" }
+            };
+    }
+}
