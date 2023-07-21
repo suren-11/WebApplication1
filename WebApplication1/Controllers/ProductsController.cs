6786431a188bed77beb241e1428f123233c66b94
suren-11 <presanth11@gmail.com>
2023-07-21 20:36:56 +0530

    Create Post

--- WebApplication1/Controllers/ProductsController.cs
+++ WebApplication1/Controllers/ProductsController.cs
@@ -11,13 +11,14 @@ namespace WebApplication1.Controllers
     public class ProductsController : ControllerBase
     {
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public ActionResult<IEnumerable<ProductDto>> GetAllProducts()
         {
             var products = ProductDB.ProductList;
             return Ok(products);
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name = "GetProduct")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -33,6 +34,28 @@ namespace WebApplication1.Controllers
                 return NotFound();
             }
             return Ok(product);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<ProductDto> CreateProduct([FromBody]ProductDto productDto)
+        {
+            if (productDto == null)
+            {
+                return BadRequest(productDto);
+            }
+            if (productDto.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            
+            var maxProductId = ProductDB.ProductList.OrderByDescending(P => P.Id).FirstOrDefault().Id;
+            productDto.Id = maxProductId + 1;
+
+            ProductDB.ProductList.Add(productDto);
+            return CreatedAtRoute("GetProduct", new { id = productDto.Id}, productDto);
         }
     }
 }
